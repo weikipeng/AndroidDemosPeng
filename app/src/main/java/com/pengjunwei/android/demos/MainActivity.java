61bb435ecae515da36f8164eee7fe1ff13095b7d
wiki <wiki.peng@qq.com>
2017-02-19 07:57:46 +0800

    init project

---
+++ app/src/main/java/com/pengjunwei/android/demos/MainActivity.java
@@ -0,0 +1,17 @@
+package com.pengjunwei.android.demos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by WikiPeng on 2017/2/19 07:51.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
